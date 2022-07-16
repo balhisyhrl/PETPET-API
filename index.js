8904363fc142016fe6ed54af09b80896ab6f47d8
balhisyhrl <iqbalhidayatsyahril@gmail.com>
2022-07-16 08:55:54 +0000

    make it better

--- index.js
+++ index.js
@@ -15,25 +15,86 @@ app.use(secure)
 app.use(express.static("assets"))
 
 app.use('/', async (req, res, next) => {
-    const petpet = req.query.url
+    if(req.query.url) {
+        const petpet = req.query.url
+        if(!petpet) return res.json({
+            status: false,
+            creator: `Balhisyhrl`,
+            github: `https://github.com/balhisyhrl`,
+            msg: "Cannot GET parameter url",
+            example: "https://anon-petpet.herokuapp.com/?url=https://upload.wikimedia.org/wikipedia/en/3/3d/480px-Gawr_Gura_-_Portrait_01.png"
+        })
+        try{
+            let animatedGif = await petPetGif(petpet)
+            let filename = "Anon-BOT-" + new Date().getTime() + + Math.floor(Math.random() * 999)+'.gif'
+            await fs.writeFileSync(`./tmp/${filename}`, animatedGif)
+            let imggiff = filename
+            res.json({
+                status: true,
+                creator: `Balhisyhrl`,
+                github: `https://github.com/balhisyhrl`,
+                result: "https://anon-petpet.herokuapp.com/?file=" + imggiff
+        })
+            //fs.unlinkSync(filename)
+        } catch(e){
+            //console.log(e)
+            res.json({
+                status: false,
+                creator: `Balhisyhrl`,
+                github: `https://github.com/balhisyhrl`,
+                msg: "Cannot GET parameter url",
+                example: "https://anon-petpet.herokuapp.com/?url=https://upload.wikimedia.org/wikipedia/en/3/3d/480px-Gawr_Gura_-_Portrait_01.png"
+        })}
+    } else if(req.query.file) {
+        const petpet = req.query.file
+        if(!petpet) return res.json({
+            status: false,
+            creator: `Balhisyhrl`,
+            github: `https://github.com/balhisyhrl`,
+            msg: "Cannot GET FILE"
+        })
+        try{
+            let filename = petpet + ".gif"
+            //await fs.writeFileSync(`./tmp/${filename}`, animatedGif)
+            let imggiff = filename
+            //express.static(__dirname + `/tmp/${imggiff}`)
+            fs.readFile(__dirname + `/tmp/${imggiff}`, function(err, data) {
+                res.writeHead(200, {'content-type':'image/gif'})
+                res.end(data)
+            })
+            //fs.unlinkSync(filename)
+        } catch(e){
+                //console.log(e)
+            res.json({
+                status: false,
+                creator: `Balhisyhrl`,
+                github: `https://github.com/balhisyhrl`,
+                msg: "Cannot GET FILE"
+            })}
+    } else {
+        res.json({
+            status: false,
+            creator: `Balhisyhrl`,
+            github: `https://github.com/balhisyhrl`,
+            msg: "Cannot GET parameter url or File",
+            example_petpet: "https://anon-petpet.herokuapp.com/?url=https://upload.wikimedia.org/wikipedia/en/3/3d/480px-Gawr_Gura_-_Portrait_01.png",
+            example_get_petpet: "https://anon-petpet.herokuapp.com/?file=Anon-BOT-165795955795822"
+        })
+    }
+})
+app.use('/petpet', async (req, res, next) => {
+    const petpet = req.query.file
     if(!petpet) return res.json({
       status: false,
       creator: `Balhisyhrl`,
       github: `https://github.com/balhisyhrl`,
-      msg: "Cannot GET parameter url",
-      example: "https://anon-petpet.herokuapp.com/?url=https://upload.wikimedia.org/wikipedia/en/3/3d/480px-Gawr_Gura_-_Portrait_01.png"
-      })
+      msg: "Cannot GET FILE"
+    })
     try{
-      let animatedGif = await petPetGif(petpet)
-      let filename = "Anon-BOT-" + new Date().getTime() + + Math.floor(Math.random() * 999)+'.gif'
-      await fs.writeFileSync(`./tmp/${filename}`, animatedGif)
+      let filename = petpet + ".gif"
+      //await fs.writeFileSync(`./tmp/${filename}`, animatedGif)
       let imggiff = filename
-      res.json({
-        status: true,
-        creator: `Balhisyhrl`,
-        github: `https://github.com/balhisyhrl`,
-        result: "https://anon-petpet.herokuapp.com/tmp/" + imggiff
-      })
+      res.sendFile(`./tmp/${imggiff}`)
     //fs.unlinkSync(filename)
     } catch(e){
       //console.log(e)
@@ -41,8 +102,7 @@ app.use('/', async (req, res, next) => {
         status: false,
         creator: `Balhisyhrl`,
         github: `https://github.com/balhisyhrl`,
-        msg: "Cannot GET parameter url",
-        example: "https://anon-petpet.herokuapp.com/?url=https://upload.wikimedia.org/wikipedia/en/3/3d/480px-Gawr_Gura_-_Portrait_01.png"
+        msg: "Cannot GET FILE"
     })}
 })
 
